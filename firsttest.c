65cb34e6656e74bfd054d796becc164db8d100d0
wuxin <star51987@163.com>
2017-10-29 02:49:20 -0700

    test 99 table

--- firsttest.c
+++ firsttest.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
 
+// 9*9 mul table
+void Table99(void)
+{
+	int i,j;
+	for (i = 0;i <= 9; i++)
+	{
+	   for (j = 1; j <= i;j++)
+	   {
+	       printf("%d*%d=%-4d",i,j,i*j);
+	   }
+	   printf("\n");
+	}
+}
+
 int iterate(int value)
 {
    if(1==value)
@@ -10,6 +24,7 @@ return iterate(value -1) + value;
 }
 int main(void)
 {
-   printf("%d\n",iterate(10));
+  // printf("%d\n",iterate(10));
+	Table99();
    return 1;
 }
